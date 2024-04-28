b0634ab2c725d8675c8c6a40342d89d40dba7bc3
Samuel Xavier <sancrisxa@gmail.com>
2024-04-28 16:12:43 -0300

    Login page

--- move-it-front-end/src/app/pages/login/login.component.ts
+++ move-it-front-end/src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,5 +10,14 @@ import { DefaultLoginLayoutComponent } from '../../components/default-login-layo
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup;
+
+  constructor() {
+    this.loginForm =new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
 
 }
